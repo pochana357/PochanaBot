d72144a5521be32a5e34d218408ebdc0cc0dfd53
pochana357 <190461112+pochana357@users.noreply.github.com>
2025-03-21 18:42:12 +0900

    Add Pochana emojis and stickers

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ const distube = new DisTube(client, {
     }
 });
 
+const emoji = { play: '<:pkm_pochiena07:1242333077129265282>', stop: '<:pkm_pochiena06:1242333081046614016>', error: '<:pkm_pochiena10:1242333629397340281>', general: '<:pkm_pochiena05:1242333078849064960>' };
+const sticker = { enter: '1277193362327474249', leave: '1277193575561429164' };
+
 client.once('ready', () => {
     logMessage(`${client.user.tag} is online and ready!`);
 });
@@ -42,7 +45,7 @@ client.on('messageCreate', async (message) => {
 
     const userVoiceChannel = message.member?.voice?.channel;
     if (!userVoiceChannel) {
-        return message.reply(`You must be in a voice channel to use commands.`);
+        return message.reply(`${emoji.error} You must be in a voice channel to use commands.`);
     }
 
     const server = userVoiceChannel.guild;
@@ -50,7 +53,7 @@ client.on('messageCreate', async (message) => {
     if (queue &&
         queue?.voice?.channel.id != userVoiceChannel.id &&
         command != 'help') {
-        return message.reply(`You must be in the bot's voice channel \`${queue?.voice?.channel.name}\` to use this command.`);
+        return message.reply(`${emoji.error} You must be in the bot's voice channel \`${queue?.voice?.channel.name}\` to use this command.`);
     }
 
     try {
@@ -60,9 +63,9 @@ client.on('messageCreate', async (message) => {
         switch (command) {
             case 'play':
 
-                if (args.length < 1) return message.reply(`Please provide a YouTube/Spotify URL or search query.`);
+                if (args.length < 1) return message.reply(`${emoji.error} Please provide a YouTube/Spotify URL or search query.`);
 
-                const loadingMsg = await message.channel.send(`Loading song. Please wait a moment...`);
+                const loadingMsg = await message.channel.send(`${emoji.general} Loading song. Please wait a moment...`);
 
                 await distube.play(userVoiceChannel, args.join(' '), {
                     textChannel: message.channel,
@@ -74,31 +77,31 @@ client.on('messageCreate', async (message) => {
                 break;
             case 'pause':
 
-                if (!queue) return message.reply(`The queue is empty, so there is nothing to pause.`);
-                if (queue.paused) return message.reply(`Music is already paused.`);
+                if (!queue) return message.reply(`${emoji.error} The queue is empty, so there is nothing to pause.`);
+                if (queue.paused) return message.reply(`${emoji.error} Music is already paused.`);
 
                 queue.pause();
 
-                message.channel.send(`Playback has been paused.`);
+                message.channel.send(`${emoji.error} Playback has been paused.`);
 
                 break;
             case 'resume':
 
-                if (!queue) return message.reply(`The queue is empty, so there is nothing to resume.`);
-                if (!queue.paused) return message.reply(`Music is already playing.`);
+                if (!queue) return message.reply(`${emoji.error} The queue is empty, so there is nothing to resume.`);
+                if (!queue.paused) return message.reply(`${emoji.error} Music is already playing.`);
 
                 queue.resume();
 
-                message.channel.send(`Playback resumed.`);
+                message.channel.send(`${emoji.play} Playback resumed.`);
 
                 break;
             case 'stop':
 
-                if (!queue) return message.reply(`There is no music playing to stop.`);
+                if (!queue) return message.reply(`${emoji.error} There is no music playing to stop.`);
 
                 queue.stop();
 
-                message.channel.send(`Music has been stopped, and the queue has been cleared.`);
+                message.channel.send(`${emoji.error} Music has been stopped, and the queue has been cleared.`);
 
                 triggerInactivity(server, queue.textChannel);
 
@@ -107,36 +110,36 @@ client.on('messageCreate', async (message) => {
 
                 distube.voices.get(server.id)?.leave();
 
-                message.channel.send(`Goodbye!`);
+                message.channel.send(`${emoji.general} Goodbye!`);
 
                 break;
             case 'skip':
 
-                if (!queue) return message.reply(`The queue is empty, so there is nothing to skip to.`);
+                if (!queue) return message.reply(`${emoji.error} The queue is empty, so there is nothing to skip to.`);
 
                 if (queue.songs.length <= 1) {
-                    return message.reply(`You are at the end of the queue, so there is nothing to skip to.`);
+                    return message.reply(`${emoji.error} You are at the end of the queue, so there is nothing to skip to.`);
                 }
 
                 await queue.skip();
-                await message.channel.send(`Skipping to the next song...`);
+                await message.channel.send(`${emoji.error} Skipping to the next song...`);
 
                 break;
             case 'rewind':
 
-                if (!queue) return message.reply(`The queue is empty, so there is nothing to rewind to.`);
+                if (!queue) return message.reply(`${emoji.error} The queue is empty, so there is nothing to rewind to.`);
 
                 if (queue.previousSongs.length <= 0) {
-                    return message.reply(`You are at the beginning of the queue, so there is nothing to rewind to.`);
+                    return message.reply(`${emoji.error} You are at the beginning of the queue, so there is nothing to rewind to.`);
                 }
 
                 await queue.previous();
-                await message.channel.send(`Rewinding to the previous song...`);
+                await message.channel.send(`${emoji.error} Rewinding to the previous song...`);
 
                 break;
             case 'queue':
 
-                if (!queue) return message.channel.send(`The queue is empty.`);
+                if (!queue) return message.channel.send(`${emoji.error} The queue is empty.`);
 
                 var queueString = `**Current Queue:**\n` +
                     queue.songs
@@ -146,14 +149,14 @@ client.on('messageCreate', async (message) => {
                 // Respect Discord message limit (2000);
                 queueString = queueString.length >= 2000 ? `${queueString.slice(0, 1997)}...` : queueString;
 
-                message.channel.send(`${queueString}`);
+                message.channel.send(`${emoji.general} ${queueString}`);
 
                 break;
             case 'debug':
 
-                if (message.author.id != getBotAdminId()) return message.reply(`Only the bot administrator may set a debug logging mode.`);
+                if (message.author.id != getBotAdminId()) return message.reply(`${emoji.error} Only the bot administrator may set a debug logging mode.`);
 
-                if (args.length < 1) return message.reply(`Setting a debug logging mode requires a value.`);
+                if (args.length < 1) return message.reply(`${emoji.error} Setting a debug logging mode requires a value.`);
 
                 const mode = args[0].toLowerCase();
 
@@ -164,26 +167,26 @@ client.on('messageCreate', async (message) => {
 
                         setDebugLoggingMode(mode);
 
-                        message.channel.send(`Debug logging mode set to \`${mode.toUpperCase()}\`.`);
+                        message.channel.send(`${emoji.general} Debug logging mode set to \`${mode.toUpperCase()}\`.`);
 
                         break;
                     default:
 
-                        return message.reply(`Invalid debug mode value.`);
+                        return message.reply(`${emoji.error} Invalid debug mode value.`);
                 }
 
                 break;
             case 'timeout':
 
-                if (args.length < 1) return message.reply(`Setting a timeout requires a value.`);
+                if (args.length < 1) return message.reply(`${emoji.error} Setting a timeout requires a value.`);
 
                 const number = parseInt(args[0]);
                 if (!isNaN(number) && number >= 0 && number <= 60) {
                     setTimeoutMinutes(server.id, number);
 
-                    message.channel.send(`Inactivity timeout set to \`${number} minutes\`.`);
+                    message.channel.send(`${emoji.general} Inactivity timeout set to \`${number} minutes\`.`);
                 } else {
-                    return message.reply(`Invalid timeout value.`);
+                    return message.reply(`${emoji.error} Invalid timeout value.`);
                 }
 
                 break;
@@ -206,8 +209,34 @@ client.on('messageCreate', async (message) => {
                 break;
         }
     } catch (error) {
-        message.reply(`An error occurred while processing your command: ${error.message}`);
+        message.reply(`${emoji.error} An error occurred while processing your command: ${error.message}`);
         logError(`${server.name}: Error in command "${message.content}":`, error);
+    }
+});
+
+// Listen for voice state updates
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    // Get a text channel to send the sticker in
+    const guildName = newState.guild.name;
+    const textChannel = newState.guild.channels.cache.find(
+        channel => channel.type === 0 && channel.name === "bot" && channel.permissionsFor(client.user).has('SendMessages')
+    );
+
+    if (!textChannel) {
+        logError(`{guildName}: Couldn't find a text channel to send the sticker in.`);
+        return;
+    }
+
+    // Check if it's the bot that joined a voice channel
+    if (newState.member.user.id === client.user.id && newState.channelId && !oldState.channelId) {
+      // Bot has joined a voice channel
+      await textChannel.send({stickers: [sticker.enter]});
+    }
+
+    // Check if it's the bot that left a voice channel
+    if (oldState.member.user.id === client.user.id && oldState.channelId && !newState.channelId) {
+      // Bot has left a voice channel
+      await textChannel.send({stickers: [sticker.leave]});
     }
 });
 
@@ -215,7 +244,7 @@ distube.on('playSong', (queue, song) => {
     const server = getServerInfo(queue);
     triggerActivity(server);
 
-    queue.textChannel.send(`🎶 Playing \`${song.name}\` - \`${song.formattedDuration}\``);
+    queue.textChannel.send(`${emoji.play} Playing \`${song.name}\` - \`${song.formattedDuration}\``);
     logMessage(`${server.name}: Playing song: ${song.name} - ${song.formattedDuration}`);
 });
 
@@ -223,7 +252,7 @@ distube.on('addSong', (queue, song) => {
     if (queue.songs.length > 1) {
         const server = getServerInfo(queue);
 
-        queue.textChannel.send(`Added \`${song.name}\` - \`${song.formattedDuration}\` to the queue!`);
+        queue.textChannel.send(`${emoji.play} Added \`${song.name}\` - \`${song.formattedDuration}\` to the queue!`);
         logMessage(`${server.name}: Added song: ${song.name} - ${song.formattedDuration}`);
     }
 });
@@ -240,7 +269,7 @@ distube.on('error', (error, queue) => {
     if (queue && queue.textChannel) {
         const server = getServerInfo(queue);
 
-        queue.textChannel.send(`Music playback encountered an error: ${error.message}`);
+        queue.textChannel.send(`${emoji.error} Music playback encountered an error: ${error.message}`);
         logError(`${server.name}: DisTube Error:`, error);
     } else {
         logError(`DisTube Error:`, error);
@@ -287,7 +316,7 @@ function triggerInactivity(server, textChannel) {
 function createInactivityTimeout(server, textChannel, minutes) {
     return setTimeout(function () {
         logMessage(`${server.name}: Inactivity timeout reached (${minutes} minutes). Bot disconnected.`);
-        textChannel.send(`Looks like you're done playing music for now. Goodbye!`);
+        textChannel.send(`${emoji.general} Looks like you're done playing music for now. Goodbye!`);
 
         distube.voices.get(server.id)?.leave();
 
